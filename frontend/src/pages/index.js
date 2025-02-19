e52517d752bbcdb35ec9de50493d038811264491
Ryan <rgwin1@ivytech.edu>
2025-02-19 18:28:25 -0500

    added files for navigation

---
+++ frontend/src/pages/index.js
@@ -0,0 +1,5 @@
+export {default as CourseList } from './courselist';
+export {default as AddCourse } from './addcourse';
+export {default as TeacherPage } from './teacherpage';
+export {default as StudentPage } from './studentpage';
+export {default as ErrorPage } from './errorpage';
